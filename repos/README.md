8f62026e800cc245aa3d946fc39e38b6a1298cf1
santosh-devQA86 <santoshdn9220@gmail.com>
2026-04-30 08:19:57 +0530

    Comminting cganges

---
+++ repos/README.md
@@ -0,0 +1,2 @@
+# Wipro_NGA_NET_FSD
+Here you can access daily uploaded .NET practice programs.
---
+++ repos/day_demo/day_demo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace day_demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
